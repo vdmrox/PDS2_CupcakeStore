8542f5e8ade4b5df8bc502e5c3912eaba259f3bf
vdmrox <vdmrox@gmail.com>
2016-04-18 22:12:21 -0300

    fixing pedido

--- src/model/domain/Pedido.java
+++ src/model/domain/Pedido.java
@@ -11,7 +11,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 @Entity
 @Table(name="TB_PEDIDO")
 public class Pedido implements Serializable{
